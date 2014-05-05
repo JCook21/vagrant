adf16c009b3825d2f9e4daff96d51e0e1d3e7d1b
Jeremy Cook <jeremycook0@gmail.com>
2014-05-04 21:52:46 -0400

    Updates to config and setup to use librarian puppet.

--- Vagrantfile
+++ Vagrantfile
@@ -7,11 +7,11 @@ Vagrant::Config.run do |config|
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = 'precise64'
+  config.vm.box = 'wheezy64'
 
   # The url from where the 'config.vm.box' box will be fetched if it
   # doesn't already exist on the user's system.
-  config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
+  config.vm.box_url = 'http://dl.dropboxusercontent.com/u/197673519/debian-7.2.0.box'
 
   # Boot with a GUI so you can see the screen. (Default is headless)
   # config.vm.boot_mode = :gui
@@ -26,8 +26,10 @@ Vagrant::Config.run do |config|
   # config.vm.share_folder "v-data", "/vagrant_data", "../data"
   config.vm.share_folder 'p-data', '/project_data', '../'
 
+  config.vm.provision :shell, :path => 'shell/bootstrap.sh'
+
   config.vm.provision :puppet do |puppet|
-    endpuppet.manifests_path = 'puppet/manifests'
+    puppet.manifests_path = 'puppet/manifests'
     puppet.manifest_file  = 'base.pp'
     puppet.module_path = 'puppet/modules'
   end
--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,31 +1,152 @@
 # Set paths to use in exec commands
 Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin' }
-exec { "apt-update":
-    command => "apt-get update"
+Class['apt'] -> Package<| |>
+Anchor['apt::source::php55'] -> Class['php'] -> Class['composer']
+
+# Dependencies
+package{'vim':
+	ensure => 'present'
+}
+package {'libevent-dev':
+	ensure => installed,
+	before => Class['php']
+}
+package {'libev-dev':
+	ensure => installed,
+	before => Class['php']
+}
+package {'pkg-config':
+	ensure => installed,
+	before => Class['php']
+}
+package {'libzmq-dev':
+	ensure => installed,
+	before => Class['php']
+}
+package {'build-essential':
+	ensure => installed,
+	before => Class['php']
+}
+package {"curl":
+	ensure => installed,
+	before => Class['php']
+}
+package {"git-core":
+	ensure => installed,
+	before => Class['php']
 }
 
-Exec["apt-update"] -> Package <| |>	
-# composer installation to be run after php modules.
-class { 'composer':
-	command_name => 'composer',
-	target_dir   => '/usr/local/bin',
-	auto_update  => true,
-	require      => Class['php_modules']
+# Add the APT module
+class{'apt':
+	always_apt_update => true
 }
-# Install composer dependencies only if a composer.json file is present.
-exec { 'composer install --dev':
-	cwd     => '/project_data',
-	require => Class['composer'],
-	onlyif  => 'test -f /project_data/composer.json'
+apt::source{'php55':
+	location   => 'http://packages.dotdeb.org',
+	release    => 'wheezy-php55',
+	repos      => 'all',
+	key        => 'E9C74FEEA2098A6E',
+	key_server => 'keys.gnupg.net'
 }
 
-# Modules to include.
-include repository
-include dependencies
-include apache
-include mysql
-include php
-include php::devel
-include php::pear
-include php_modules
-include webserver
+# Apache config
+class{'apache':
+	mpm_module    => 'prefork'
+}
+
+class{'apache::mod::php':}
+class{'apache::mod::rewrite':}
+apache::vhost{ 'example.dev':
+	port    => '80',
+	docroot => '/vagrant/web'
+}
+
+# Mysql server
+include '::mysql::server'
+
+# PHP config
+include 'php'
+
+php::module{'mysql':}
+php::module { "apcu":
+	module_prefix => "php5-"
+}
+php::module { "imagick": }
+php::module { "gd": }
+php::module { "mcrypt": }
+php::module { "memcache": }
+php::module { "pspell": }
+php::module { "snmp": }
+php::module { "sqlite": }
+php::module { "xmlrpc": }
+php::module { "xsl": }
+php::module { "curl": }
+php::pecl::module { "xdebug": }
+php::pecl::module { "libevent":
+	preferred_state => "beta",
+	use_package     => 'false'
+}
+php::pecl::module { 'zmq':
+	preferred_state => "beta",
+	use_package     => 'false'
+}
+exec { 'git clone --recursive https://github.com/m4rw3r/php-libev && cd php-libev && phpize && ./configure --with-libev && make && make install':
+	cwd     => '/tmp',
+	require => [Package['git-core'], Package['php5-dev'], Package['libev-dev']],
+	alias   => 'git-libev'
+}
+
+# Setting a default timezone avoids warning about date default timezone
+php::augeas{
+	'php-date_timezone_apache':
+		entry   => 'Date/date.timezone',
+		value   => 'UTC',
+		notify  => Service['apache2'],
+		require => Class['php'];
+	'php-date_timezone_cli':
+		entry   => 'Date/date.timezone',
+		value   => 'UTC',
+		require => Class['php'],
+		target  => '/etc/php5/cli/php.ini',
+}
+
+# Composer config
+class{'composer':
+	target_dir      => '/usr/local/bin',
+	composer_file   => 'composer',
+	suhosin_enabled => false
+}
+
+# Config files for custom compiled PECL modules
+file {'/etc/php5/cli/libevent.ini':
+	path    => '/etc/php5/cli/conf.d/libevent.ini',
+	content => 'extension=libevent.so',
+	require => Exec['pecl-libevent'],
+}
+file {'/etc/php5/cli/zmq.ini':
+	path    => '/etc/php5/cli/conf.d/zmq.ini',
+	content => 'extension=zmq.so',
+	require => Exec['pecl-zmq'],
+}
+file {'/etc/php5/apache2/libevent.ini':
+	path    => '/etc/php5/apache2/conf.d/libevent.ini',
+	content => 'extension=libevent.so',
+	require => Exec['pecl-libevent'],
+	notify  => Service['apache2'],
+}
+file {'/etc/php5/apache2/zmq.ini':
+	path    => '/etc/php5/apache2/conf.d/zmq.ini',
+	content => 'extension=zmq.so',
+	require => Exec['pecl-zmq'],
+	notify  => Service['apache2'],
+}
+file {'/etc/php5/cli/libev.ini':
+	path    => '/etc/php5/cli/conf.d/libev.ini',
+	content => 'extension=libev.so',
+	require => Exec['git-libev'],
+}
+file {'/etc/php5/apache2/libev.ini':
+	path    => '/etc/php5/apache2/conf.d/libev.ini',
+	content => 'extension=libev.so',
+	require => Exec['git-libev'],
+	notify  => Service['apache2']
+}
